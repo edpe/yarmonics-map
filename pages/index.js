179dd03cdd95d1cab62a07a042720e8d4037ad88
edpe <ed@edperkins.co.uk>
2021-11-01 13:46:58 +0000

    feat: add sentry

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import dynamic from "next/dynamic";
+import { initializeApp } from "firebase/app";
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import { SpinnerRoundOutlined } from "spinners-react";
 
 import styled from "styled-components";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2yS1qiHxOvtZ3JHX4q8LmviQbE2CJgKs",
+  authDomain: "yarmonics-map.firebaseapp.com",
+  projectId: "yarmonics-map",
+  storageBucket: "yarmonics-map.appspot.com",
+  messagingSenderId: "188355762607",
+  appId: "1:188355762607:web:67f9c39ff65efadfbf775a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const CenterInPage = styled.div`
   height: 100vh;
