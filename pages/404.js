b3499479964b4bd7ac9c8ef607b36c95d9190b44
edpe <ed@edperkins.co.uk>
2021-11-08 09:37:35 +0000

    feat: custom error pages

---
+++ pages/404.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "../src/components/Layout";
+import styles from "../styles/ErrorPage.module.css";
+
+const FourHundredPage = () => {
+  return (
+    <Layout>
+      <div className={styles.centerInPage}>
+        <div className={styles.title}>
+          We're very sorry but we can't find the page you're looking for
+        </div>
+        <Image
+          className={styles.errorImage}
+          src="/thinking.svg"
+          alt="person sleeping"
+          width={300}
+          height={168}
+        />
+        <p className={styles.text}>
+          Try going to
+          <Link href="/"> the landing page</Link> instead
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default FourHundredPage;
---
+++ pages/500.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "../src/components/Layout";
+import styles from "../styles/ErrorPage.module.css";
+
+const FiveHundredPage = () => {
+  return (
+    <Layout>
+      <div className={styles.centerInPage}>
+        <div className={styles.title}>
+          We're very sorry but the map has been asleep . . .
+        </div>
+
+        <Image
+          className={styles.errorImage}
+          src="/sleeping.svg"
+          alt="person sleeping"
+          width={300}
+          height={168}
+        />
+
+        <p className={styles.text}>
+          This app goes to sleep to save energy when it's not been needed for a
+          while
+        </p>
+        <p className={styles.text}>
+          It should have woken up by now so please{" "}
+          <Link href="/">click here to refresh the page</Link> or hit refresh on
+          your browser and all should be fine
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default FiveHundredPage;
